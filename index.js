948e1806cc986fc2ebefefd94007dff1abcd5fa7
Derek <derekylee@outlook.com>
2022-09-09 15:44:35 -0700

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+// const path = require('path');
+const router = require('./router');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+app.set('port', PORT);
+app.set('env', NODE_ENV);
+
+app.use(logger('tiny'));
+
+app.use(express.json());
+const corsOptions = {
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.use(router)
+// app.use('/', require(path.join(__dirname, 'router')));
+
+app.use((req, res, next) => {
+  const err = new Error(`${req.method} ${req.url} Not Found`);
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Express Server started on Port ${app.get(
+      'port'
+    )} | Environment : ${app.get('env')}`
+  );
+});
---
+++ router.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const multer = require('multer')
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/images')
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.replaceAll(' ','');
+    const fileShort = fileName.slice(0, fileName.indexOf('.'));
+    const fileType = fileName.substring(fileName.indexOf('.'));
+    cb(null, fileShort + '-' + Date.now() + fileType)
+  }
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 10000000,
+  },
+
+  fileFilter(res, file, cb) {
+    if (
+      !file.originalname.endsWith('.jpg') &&
+      !file.originalname.endsWith('.jpeg') &&
+      !file.originalname.endsWith('.png')
+    ) {
+      return cb(new Error('Please upload a valid image!'));
+    }
+    cb(undefined, true);
+  },
+
+});
+
+router.get('/', (req, res) => {
+  res.send('default response achieved')
+})
+
+router.post('/upload', upload.single('picture'), (req, res) => {
+  const filename = req?.file.filename;
+  res.status(200).send(req.headers.host + '/images/' + filename);
+});
+
+// router.get('/images/:image', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'images', req.params.image));
+// });
+
+module.exports = router;

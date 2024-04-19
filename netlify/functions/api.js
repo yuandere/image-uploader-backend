ab6021e2ef56470062a46affaf009be94b609d24
Derek <derekylee@outlook.com>
2024-04-18 22:05:28 -0700

    cors fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,12 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import cors from "cors";
 
 const api = express();
-
 const router = Router();
+
+api.use(cors());
+
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);

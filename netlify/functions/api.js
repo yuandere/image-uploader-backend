83e4051274c01146c919e40ca7c52c4f6769e681
Derek <derekylee@outlook.com>
2024-04-18 21:16:39 -0700

    migration wip

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,6 +6,6 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use('/.netlify/functions/', router);
+api.use("/api/", router);
 
 export const handler = serverless(api);
